9f61b763e49ee9449ff0200649e450a5506c93ff
zrosin <77403498+zrosin@users.noreply.github.com>
2021-09-21 20:05:36 -0500

    Finish Procrastination
    1. Fixed Image wackiness
    2. Added (stole) inversion code

--- Photo-Editor/Photo-Editor/PhotoEditor.Designer.cs
+++ Photo-Editor/Photo-Editor/PhotoEditor.Designer.cs
@@ -37,24 +37,26 @@
             // 
             this.pictureBox.Location = new System.Drawing.Point(12, 12);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(533, 337);
+            this.pictureBox.Size = new System.Drawing.Size(379, 326);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             // 
             // InvertImage
             // 
-            this.InvertImage.Location = new System.Drawing.Point(247, 461);
+            this.InvertImage.Location = new System.Drawing.Point(156, 459);
             this.InvertImage.Name = "InvertImage";
             this.InvertImage.Size = new System.Drawing.Size(75, 23);
             this.InvertImage.TabIndex = 1;
             this.InvertImage.Text = "Invert";
             this.InvertImage.UseVisualStyleBackColor = true;
+            this.InvertImage.Click += new System.EventHandler(this.InvertImage_Click);
             // 
             // PhotoEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(557, 496);
+            this.ClientSize = new System.Drawing.Size(403, 494);
             this.Controls.Add(this.InvertImage);
             this.Controls.Add(this.pictureBox);
             this.Name = "PhotoEditor";
