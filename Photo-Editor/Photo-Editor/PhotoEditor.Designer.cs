2a41befa48879ad2c981f5062ac27f7b86c1bead
zrosin <77403498+zrosin@users.noreply.github.com>
2021-10-03 16:11:30 -0500

    Tint Functionality

--- Photo-Editor/Photo-Editor/PhotoEditor.Designer.cs
+++ Photo-Editor/Photo-Editor/PhotoEditor.Designer.cs
@@ -30,7 +30,11 @@
         {
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.InvertImage = new System.Windows.Forms.Button();
+            this.tintImage = new System.Windows.Forms.Button();
+            this.brightnessBar = new System.Windows.Forms.TrackBar();
+            this.tintPicker = new System.Windows.Forms.ColorDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessBar)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox
@@ -52,17 +56,38 @@
             this.InvertImage.UseVisualStyleBackColor = true;
             this.InvertImage.Click += new System.EventHandler(this.InvertImage_Click);
             // 
+            // tintImage
+            // 
+            this.tintImage.Location = new System.Drawing.Point(278, 459);
+            this.tintImage.Name = "tintImage";
+            this.tintImage.Size = new System.Drawing.Size(75, 23);
+            this.tintImage.TabIndex = 2;
+            this.tintImage.Text = "Tint";
+            this.tintImage.UseVisualStyleBackColor = true;
+            this.tintImage.Click += new System.EventHandler(this.tintImage_Click);
+            // 
+            // brightnessBar
+            // 
+            this.brightnessBar.Location = new System.Drawing.Point(12, 459);
+            this.brightnessBar.Name = "brightnessBar";
+            this.brightnessBar.Size = new System.Drawing.Size(104, 45);
+            this.brightnessBar.TabIndex = 3;
+            // 
             // PhotoEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(403, 494);
+            this.Controls.Add(this.brightnessBar);
+            this.Controls.Add(this.tintImage);
             this.Controls.Add(this.InvertImage);
             this.Controls.Add(this.pictureBox);
             this.Name = "PhotoEditor";
             this.Text = "PhotoEditor";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessBar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,5 +95,8 @@
 
         private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Button InvertImage;
+        private System.Windows.Forms.Button tintImage;
+        private System.Windows.Forms.TrackBar brightnessBar;
+        private System.Windows.Forms.ColorDialog tintPicker;
     }
 }
--- Photo-Editor/Photo-Editor/PhotoEditor.cs
+++ Photo-Editor/Photo-Editor/PhotoEditor.cs
@@ -30,18 +30,67 @@ namespace Photo_Editor
             pictureBox.Image = Image.FromFile(ImageLocation);
         }
 
-        //Stolen from McCown's code
+        
         private void InvertImage_Click(object sender, EventArgs e)
         {
-            var transformedBitmap = new Bitmap(pictureBox.Image);
+            var newImage = new Bitmap(pictureBox.Image);
 
             // This could take a long time... should be done in a thread
-            InvertColors(transformedBitmap);
+            InvertColors(newImage);
 
-            pictureBox.Image = transformedBitmap;
+            pictureBox.Image = newImage;
         }
 
-        //Also stolen from McCown
+        private void tintImage_Click(object sender, EventArgs e)
+        {
+            var newImage = new Bitmap(pictureBox.Image);
+
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.AllowFullOpen = true;
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                AlterColors(newImage, colorDialog.Color);
+            }
+
+            pictureBox.Image = newImage;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //McCown's code
         private void InvertColors(Bitmap transformedBitmap)
         {
             for (int y = 0; y < transformedBitmap.Height; y++)
@@ -57,5 +106,45 @@ namespace Photo_Editor
                 }
             }
         }
+
+        //McCown's code
+        private void AlterColors(Bitmap transformedBitmap, Color chosenColor)
+        {
+            for (int y = 0; y < transformedBitmap.Height; y++)
+            {
+                for (int x = 0; x < transformedBitmap.Width; x++)
+                {
+                    var color = transformedBitmap.GetPixel(x, y);
+                    int ave = (color.R + color.G + color.B) / 3;
+                    double percent = ave / 255.0;
+                    int newRed = Convert.ToInt32(chosenColor.R * percent);
+                    int newGreen = Convert.ToInt32(chosenColor.G * percent);
+                    int newBlue = Convert.ToInt32(chosenColor.B * percent);
+                    var newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                    transformedBitmap.SetPixel(x, y, newColor);
+                }
+            }
+        }
+
+        //McCown's code
+        private void ChangeBrightness(Bitmap transformedBitmap, int brightness)
+        {
+            // Calculate amount to change RGB
+            int amount = Convert.ToInt32(2 * (50 - brightness) * 0.01 * 255);
+            for (int y = 0; y < transformedBitmap.Height; y++)
+            {
+                for (int x = 0; x < transformedBitmap.Width; x++)
+                {
+                    var color = transformedBitmap.GetPixel(x, y);
+                    int newRed = Math.Max(0, Math.Min(color.R - amount, 255));
+                    int newGreen = Math.Max(0, Math.Min(color.G - amount, 255));
+                    int newBlue = Math.Max(0, Math.Min(color.B - amount, 255));
+                    var newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                    transformedBitmap.SetPixel(x, y, newColor);
+                }
+            }
+        }
+
+       
     }
 }
