e7e72755e74e9720e6be50c94ad1dae89dec68f0
15jralls <jralls@harding.edu>
2021-10-04 14:29:13 -0500

    Split Controller and Tree view
    I made the split controller and menu bar.
    I also made and populated the Tree view

--- Photo-Editor/Photo-Editor/PhotoBrowser.Designer.cs
+++ Photo-Editor/Photo-Editor/PhotoBrowser.Designer.cs
@@ -30,51 +30,147 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhotoBrowser));
             this.PictureList = new System.Windows.Forms.ListView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.locateOnDiskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectRootFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.veiwToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // PictureList
             // 
+            this.PictureList.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PictureList.HideSelection = false;
-            this.PictureList.Location = new System.Drawing.Point(298, 12);
+            this.PictureList.Location = new System.Drawing.Point(0, 0);
             this.PictureList.MultiSelect = false;
             this.PictureList.Name = "PictureList";
-            this.PictureList.Size = new System.Drawing.Size(490, 426);
+            this.PictureList.Size = new System.Drawing.Size(476, 401);
             this.PictureList.TabIndex = 0;
             this.PictureList.UseCompatibleStateImageBehavior = false;
             this.PictureList.ItemActivate += new System.EventHandler(this.LaunchEditor);
             // 
-            // label1
+            // splitContainer1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Current Directory";
+            this.splitContainer1.Location = new System.Drawing.Point(0, 53);
+            this.splitContainer1.Name = "splitContainer1";
             // 
-            // label2
+            // splitContainer1.Panel1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 37);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.PictureList);
+            this.splitContainer1.Size = new System.Drawing.Size(803, 401);
+            this.splitContainer1.SplitterDistance = 323;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(323, 401);
+            this.treeView1.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.veiwToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.locateOnDiskToolStripMenuItem,
+            this.selectRootFolderToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // locateOnDiskToolStripMenuItem
+            // 
+            this.locateOnDiskToolStripMenuItem.Name = "locateOnDiskToolStripMenuItem";
+            this.locateOnDiskToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.locateOnDiskToolStripMenuItem.Text = "Locate on Disk";
+            // 
+            // selectRootFolderToolStripMenuItem
+            // 
+            this.selectRootFolderToolStripMenuItem.Name = "selectRootFolderToolStripMenuItem";
+            this.selectRootFolderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.selectRootFolderToolStripMenuItem.Text = "Select Root Folder";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // veiwToolStripMenuItem
+            // 
+            this.veiwToolStripMenuItem.Name = "veiwToolStripMenuItem";
+            this.veiwToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.veiwToolStripMenuItem.Text = "Veiw";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 24);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(766, 23);
+            this.progressBar1.TabIndex = 5;
             // 
             // PhotoBrowser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.PictureList);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "PhotoBrowser";
             this.Text = "Photo Browser";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,8 +179,17 @@
         #endregion
 
         private System.Windows.Forms.ListView PictureList;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem locateOnDiskToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectRootFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem veiwToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- Photo-Editor/Photo-Editor/PhotoBrowser.cs
+++ Photo-Editor/Photo-Editor/PhotoBrowser.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Photo_Editor
 {
@@ -17,37 +18,62 @@ namespace Photo_Editor
         private string photoDirectory;
         private List<FileInfo> photoFiles;
         public ListBox.ObjectCollection Items { get; }
+        private CancellationTokenSource cancellationTokenSource;
 
         //This does a thing
         public PhotoBrowser()
         {
             InitializeComponent();
-
+            
+            //makes the initial directory the MyPictures directory
             photoDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-            label2.Text = photoDirectory;
 
-            PopulateImages();
+            //loads the initial directory
+            loadDirectory();
         }
 
-        private void PopulateImages()
+        //loads a directory
+        private async void loadDirectory()
         {
-            photoFiles = new List<FileInfo>();
-            DirectoryInfo homeDir = new DirectoryInfo(photoDirectory);
+            ListDirectory(treeView1, photoDirectory);
+            await PopulateImages();
+        }
 
-            foreach (FileInfo file in homeDir.GetFiles("*.jpg"))
+        //Populates the listview with the images in the selected directory
+        private async Task PopulateImages()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+
+            await Task.Run(() =>
             {
-                photoFiles.Add(file);
-                PictureList.Items.Add(file.ToString());
-            }
-            foreach (FileInfo file in homeDir.GetFiles("*.jpeg"))
-            {
-                photoFiles.Add(file);
-                PictureList.Items.Add(file.ToString());
-            }
+                photoFiles = new List<FileInfo>();
+                DirectoryInfo homeDir = new DirectoryInfo(photoDirectory);
+
+                foreach (FileInfo file in homeDir.GetFiles("*.jpg"))
+                {
+                       photoFiles.Add(file);
+                       PictureList.Items.Add(file.ToString());
+
+
+                    //Stop looping if a new directory is selected
+                    if (cancellationTokenSource.Token.IsCancellationRequested)
+                        break;
+                }
+                foreach (FileInfo file in homeDir.GetFiles("*.jpeg"))
+                { 
+                        photoFiles.Add(file);
+                        PictureList.Items.Add(file.ToString());
+
+                    //Stop looping if a new directory is selected
+                    if (cancellationTokenSource.Token.IsCancellationRequested)
+                        break;
+                }
+            });
+           
         }
 
 
-
+        //Opens the photo editor
         private void LaunchEditor(object sender, EventArgs e)
         {
             string x = photoDirectory + "\\" + PictureList.SelectedItems[0].Text;
@@ -55,6 +81,34 @@ namespace Photo_Editor
             PhotoEditor photoEditor = new PhotoEditor(x);
 
             photoEditor.ShowDialog();
+        }
+
+        //The next two functions work together to poulate the treeview 
+        //From https://stackoverflow.com/questions/6239544/populate-treeview-with-file-system-directory-structure
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        //From https://stackoverflow.com/questions/6239544/populate-treeview-with-file-system-directory-structure
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            return directoryNode;
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
